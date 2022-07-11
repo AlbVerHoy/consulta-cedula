6eaa8abc224a31e417030e544fd124f00cb39d54
AlbVerHoy <albertovh93@outlook.com>
2022-07-10 19:11:16 -0500

    Update DigitalDocuments.js

--- src/pages/DigitalDocuments.js
+++ src/pages/DigitalDocuments.js
@@ -10,13 +10,31 @@ import useMediaQuery from '@mui/material/useMediaQuery';
 import Breadcrumbs from '@mui/material/Breadcrumbs';
 import HomeIcon from '@mui/icons-material/Home';
 import LightbulbIcon from '@mui/icons-material/Lightbulb';
+import Table from '@mui/material/Table';
+import TableBody from '@mui/material/TableBody';
+import TableCell from '@mui/material/TableCell';
+import TableContainer from '@mui/material/TableContainer';
+import TableHead from '@mui/material/TableHead';
+import TableRow from '@mui/material/TableRow';
+import Paper from '@mui/material/Paper';
 import { FindAllPersons } from '../services/personService';
 
 export default function DigitalDocuments() {
 	const [folio, setFolio] = useState('');
 	const [url, setUrl] = useState('');
 	const [showUrl, setShowUrl] = useState(false);
+	const [rows, setRows] = useState([]);
 	const matchesWidth = useMediaQuery('(min-width:1170px)');
+
+	const headerStyles = {
+		fontSize: '18px',
+		fontWeight: 600,
+	};
+
+	const rowStyles = {
+		fontSize: '18px',
+		fontWeight: 400,
+	};
 
 	const onSearchHandler = () => {
 		setUrl('');
@@ -24,6 +42,7 @@ export default function DigitalDocuments() {
 		FindAllPersons().then((res) => {
 			var result = res.find((person) => person.folio === folio && person.url);
 			if (result) {
+				setRows([result]);
 				setUrl(result.url);
 				setShowUrl(true);
 			}
@@ -40,7 +59,7 @@ export default function DigitalDocuments() {
 		<Box
 			sx={{
 				marginTop: '60px',
-				height: matchesWidth ? '600px' : '800px',
+				height: matchesWidth ? '700px' : '1000px',
 				backgroundColor: 'divider',
 			}}>
 			<div
@@ -200,9 +219,50 @@ export default function DigitalDocuments() {
 							después de su emisión.
 						</Container>
 						{showUrl ? (
-							<Link href={url} target="_blank">
-								Descargar
-							</Link>
+							<TableContainer component={Paper} sx={{ marginTop: '20px' }}>
+								<Table sx={{ minWidth: 650 }} aria-label="simple table">
+									<TableHead>
+										<TableRow>
+											<TableCell sx={headerStyles}>Folio</TableCell>
+											<TableCell sx={headerStyles}>Nombre</TableCell>
+											<TableCell sx={headerStyles}>Primer apellido</TableCell>
+											<TableCell sx={headerStyles}>Segundo apellido</TableCell>
+											<TableCell sx={headerStyles}>Institución</TableCell>
+											<TableCell sx={headerStyles}>Carrera</TableCell>
+											<TableCell sx={headerStyles}>Descargar</TableCell>
+										</TableRow>
+									</TableHead>
+									<TableBody>
+										{rows ? (
+											rows.map((row) => (
+												<TableRow
+													key={row.firstName}
+													sx={{
+														'&:last-child td, &:last-child th': { border: 0 },
+													}}>
+													<TableCell component="th" scope="row" sx={rowStyles}>
+														{row.folio}
+													</TableCell>
+													<TableCell sx={rowStyles}>{row.firstName}</TableCell>
+													<TableCell sx={rowStyles}>{row.lastName}</TableCell>
+													<TableCell sx={rowStyles}>
+														{row.secondLastName}
+													</TableCell>
+													<TableCell sx={rowStyles}>{row.university}</TableCell>
+													<TableCell sx={rowStyles}>{row.career}</TableCell>
+													<TableCell sx={rowStyles}>
+														<Link href={url} target="_blank">
+															Descargar
+														</Link>
+													</TableCell>
+												</TableRow>
+											))
+										) : (
+											<></>
+										)}
+									</TableBody>
+								</Table>
+							</TableContainer>
 						) : (
 							<></>
 						)}
